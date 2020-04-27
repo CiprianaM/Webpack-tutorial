daafcc3faf7fb013e4abd8285e7a54361477d618
CiprianaM <cipriana2001@gmail.com>
2020-04-27 16:01:42 +0200

    generated bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+//old way of importing files because in webpack.config files we cannot use ES6
+
+const path = require ('path');
+
 module.exports = {
   // entry point = the first script webpack should look at
-  entry: "index.js",
+  entry: "./src/index.js",
   //output: the name of the bundled js file webpack should create, along with its location
   //if the location doesn't exist, webpack will create it
   output: {
     filename: "bundle.js",
-    path: "./dist"
+    //instead of "dist", we add path.resolve which will provide as output the absolute path
+    path: path.resolve(__dirname, './dist')
   },
   mode: "none"
 }
